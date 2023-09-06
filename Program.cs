e05af2b43010ffd8d07448fe253f77773a309398
Grzegorz <hvitr.ulfr@gmail.com>
2023-09-06 13:23:23 +0200

    3. Registered Serilog logging system writing to file and Seq localhost server.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -16,6 +18,8 @@ builder.Services.AddCors(options =>
         .AllowAnyMethod()
         );
 });
+
+builder.Host.UseSerilog((ctx, lc) => lc.WriteTo.Console().ReadFrom.Configuration(ctx.Configuration));
 
 var app = builder.Build();
 
