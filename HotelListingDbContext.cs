26ec68a91f625505d01063220c90273ce9f37a89
Grzegorz <hvitr.ulfr@gmail.com>
2023-09-06 15:21:46 +0200

    5. Setup Entity Framework
    Add the connection string and app db context in API project.

---
+++ HotelListingDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HotelListing.API
+{
+    public class HotelListingDbContext : DbContext
+    {
+        public HotelListingDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
