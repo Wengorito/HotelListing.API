140e56c320cb2d0d0cd570eacfb5a4ced1b2ec7b
Grzegorz <hvitr.ulfr@gmail.com>
2023-09-08 16:13:13 +0200

    13. Seeded Identity Roles. Refactored previous seedings from DbContext to Configuration classed (Separation of Concerns).

---
+++ Data/Configurations/HotelConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace HotelListing.API.Data.Configurations
+{
+    public class HotelConfiguration : IEntityTypeConfiguration<Hotel>
+    {
+        public void Configure(EntityTypeBuilder<Hotel> builder)
+        {
+            builder.HasData(
+                new Hotel
+                {
+                    Id = 1,
+                    Name = "Sandals Hotel and Spa",
+                    Address = "Negril",
+                    CountryId = 1,
+                    Rating = 4.5
+                },
+                new Hotel
+                {
+                    Id = 2,
+                    Name = "Comfort Suites",
+                    Address = "GeorgeTown",
+                    CountryId = 3,
+                    Rating = 4.3
+                },
+                new Hotel
+                {
+                    Id = 3,
+                    Name = "Grand Palladium",
+                    Address = "Nassua",
+                    CountryId = 2,
+                    Rating = 4
+                }
+            );
+        }
+    }
+}
---
+++ Migrations/20230908141051_SeedDefaultRoles.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace HotelListing.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedDefaultRoles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "f2697e7a-5b36-4fd8-b411-af9569ca859f", null, "User", "USER" },
+                    { "f57007c2-7d90-449b-9faf-0c1d8f4c310e", null, "Administrator", "ADMINISTRATOR" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "f2697e7a-5b36-4fd8-b411-af9569ca859f");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "f57007c2-7d90-449b-9faf-0c1d8f4c310e");
+        }
+    }
+}
